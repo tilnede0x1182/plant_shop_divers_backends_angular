e6960fa3e1b12cb725a51e7ca3a74ca03d5d6af1
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-11-02 18:49:59 +0100

    V1.3

--- apps/plant_shop_java/plant_shop_java_quarkus/src/security/CdiRequestScopeFilter.java
+++ apps/plant_shop_java/plant_shop_java_quarkus/src/security/CdiRequestScopeFilter.java
@@ -3,36 +3,39 @@ package security;
 import jakarta.enterprise.context.control.RequestContextController;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.container.PreMatching;
 import jakarta.ws.rs.ext.Provider;
+import jakarta.annotation.Priority;
 import java.io.IOException;
 
 @Provider
+@PreMatching
+@Priority(Priorities.AUTHENTICATION)
 @Singleton
 public class CdiRequestScopeFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
-    private static final ThreadLocal<Boolean> ACTIVE = ThreadLocal.withInitial(() -> Boolean.FALSE);
+    private static final String CDI_ACTIVE_KEY = CdiRequestScopeFilter.class.getName() + ".active";
 
     @Inject
     RequestContextController requestContextController;
 
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        if (requestContextController.activate()) {
-            ACTIVE.set(Boolean.TRUE);
-        } else {
-            ACTIVE.set(Boolean.FALSE);
-        }
+        boolean activated = requestContextController.activate();
+        requestContext.setProperty(CDI_ACTIVE_KEY, activated);
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        if (ACTIVE.get()) {
+        Object flag = requestContext.getProperty(CDI_ACTIVE_KEY);
+        if (flag instanceof Boolean && (Boolean) flag) {
             requestContextController.deactivate();
         }
-        ACTIVE.remove();
+        requestContext.removeProperty(CDI_ACTIVE_KEY);
     }
 }

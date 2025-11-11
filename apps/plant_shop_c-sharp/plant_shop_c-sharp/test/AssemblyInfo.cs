39e38e65147a09743fa645bca394be69519d493e
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-11-11 01:28:46 +0100

    Debug backend c-sharp sans framework - V0.1.2

---
+++ apps/plant_shop_c-sharp/plant_shop_c-sharp/test/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyCompany("plant_shop_c-sharp.Tests")]
+[assembly: AssemblyProduct("plant_shop_c-sharp.Tests")]
+[assembly: AssemblyTitle("plant_shop_c-sharp.Tests")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: TargetFramework(".NETCoreApp,Version=v8.0", FrameworkDisplayName = ".NET 8.0")]

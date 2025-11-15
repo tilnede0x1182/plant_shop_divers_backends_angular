0df7f71c5f5235136099f60143b96a38b723494e
Cyrille DE GASQUET <pro.cyrille.degasquet@gmail.com>
2025-11-15 10:02:39 +0100

    Microservices compilent 2

--- apps/plant_shop_java/Structure_en_micro_services/plant_shop_java_spring_microservices/plant_shop_java_spring_boot_security_hibernate_microservices/user-service/src/controllers/UserController.java
+++ apps/plant_shop_java/Structure_en_micro_services/plant_shop_java_spring_microservices/plant_shop_java_spring_boot_security_hibernate_microservices/user-service/src/controllers/UserController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import model.User;
-import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
